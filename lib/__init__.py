576e14b23a87cc967c768f31db13b80779ebd19a
nils-e <nrognerud@shopstyle.com>
2022-09-18 12:26:52 +0800

    fix: cleaning up lib __init__ comments

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,18 +1,3 @@
-# from .application import Application
-# from .assertions import assert_selector, assert_no_selector
-# from .configuration import Configuration
-# from .driver import SpectronDriver
-# from .element import wrap_element, SpectronElement
-# from .exception import *
-# from .expected import text_to_be_present_in_element, title_contains
-# from .finders import *
-# from .globals import *
-# from .helper import to_seconds, generate_target
-# from .logging import set_selenium_log_level
-# from .matchers import Title, URL, Element
-# from .query import query
-# from .result import ResultDict, Result
-
 from . import application
 from . import assertions
 from . import configuration
